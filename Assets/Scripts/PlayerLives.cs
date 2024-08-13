eccb0dc154fc8491b67b86f1a58d6cdef32c7568
Brampie <bram.lippens09@gmail.com>
2024-08-13 15:57:36 +0200

    enemies damage

---
+++ Assets/Scripts/PlayerLives.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerLives : MonoBehaviour
+{
+    public int lives = 3;
+    public Image[] livesUi;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.gameObject.CompareTag("Fighter") || collision.collider.gameObject.CompareTag("Scout"))
+        {
+            Destroy(collision.collider.gameObject);
+            lives -= 1;
+            for (int i = 0; i < livesUi.Length; i++){
+                if(i < lives)
+                {
+                    livesUi[i].enabled = true;
+                }
+                else
+                {
+                    livesUi[i].enabled = false;
+                }
+            }
+            if(lives <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     public float moveSpeed = 2;
+    private Animator destroyFighter;
+    private Animator destroyScout;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -18,10 +20,42 @@ public class Projectile : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "Enemy")
+        if (collision.gameObject.CompareTag("Fighter") || collision.gameObject.CompareTag("Scout"))
         {
-            Destroy(collision.gameObject);
+            Animator enemyAnimator = collision.gameObject.GetComponent<Animator>();
+            if (enemyAnimator != null)
+            {
+                if (collision.gameObject.CompareTag("Fighter"))
+                {
+                    enemyAnimator.Play("DestroyFighter");
+                }
+                else if (collision.gameObject.CompareTag("Scout"))
+                {
+                    enemyAnimator.Play("DestroyScout");
+                }
+
+                Destroy(collision.gameObject, enemyAnimator.GetCurrentAnimatorStateInfo(0).length + 0.75f);
+            }
+            else
+            {
+                Destroy(collision.gameObject); // If no animator, destroy immediately
+            }
+
+            Destroy(gameObject); // Destroy the current gameObject (the one this script is attached to)
+        }
+        else if (collision.gameObject.CompareTag("Boundary"))
+        {
             Destroy(gameObject);
         }
     }
+
+    private IEnumerator DestroyAfterDelay(GameObject objectToDestroy, float delay)
+    {
+        // Wait for the specified delay
+        yield return new WaitForSeconds(delay);
+
+        // Destroy the object after the delay
+        Destroy(objectToDestroy);
+    }
+
 }
